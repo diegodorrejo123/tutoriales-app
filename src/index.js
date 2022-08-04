8a032506956fa620ee6d75ab0bec6debd6acd56b
Diego Dorrejo <dorrejo14@gmail.com>
2022-08-03 21:12:31 -0400

    agregado cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('./db/mongoose')
+var cors = require('cors')
 const categoryRouter = require('./routers/category');
 const postRouter = require('./routers/post');
 
 const app = express()
+app.use(cors())
 const port = process.env.PORT || 3000
 
 app.use(express.json())
